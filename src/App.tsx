19446513746bbf49e187d218028eeb50be0fd69e
Napoleon Salazar <nsalazar@coolfiresolutions.com>
2020-11-30 14:19:09 -0600

    * Move BrowserRouter to app * Skip test

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
+import { BrowserRouter } from 'react-router-dom'
 import { AppNavigation } from './features/commons/AppNavigation'
 import { AppRoutes } from './features/commons/AppRoutes'
 
 
 function App() {
   return (
-    <div className="grid grid-cols-10">
-      <div className="col-span-2 bg-gray-100">
-        <AppNavigation />
+    <BrowserRouter>
+      <div className="grid grid-cols-10">
+        <div className="col-span-2 bg-gray-100">
+          <AppNavigation />
+        </div>
+        <div className="col-span-8">
+          <AppRoutes />
+        </div>
       </div>
-      <div className="col-span-8">
-        <AppRoutes />
-      </div>
-    </div>
+    </BrowserRouter>
   )
 }
 
