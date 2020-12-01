1faf80fb4157769a6161141d8cbb0952d23c8116
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2020-12-01 09:12:23 -0600

    add auth provider

---
+++ src/features/auth/AuthProvider.tsx
@@ -0,0 +1,52 @@
+import {createContext, useContext, useEffect, useState} from 'react'
+import {auth} from '../../firebase'
+
+type User = typeof auth.currentUser
+
+interface Auth {
+  signIn(email: string, password: string): Promise<User>
+  user: User
+  loading: boolean
+}
+
+const AuthContext = createContext<Auth>((undefined as unknown) as Auth)
+
+export function AuthProvider({children}: {children: React.ReactNode}) {
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState<User>(null)
+
+  async function signIn(email: string, password: string) {
+    const {user} = await auth.signInWithEmailAndPassword(email, password)
+    setUser(user)
+    return user
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user)
+      } else {
+        setUser(null)
+      }
+      setLoading(false)
+    })
+
+    return () => unsubscribe()
+  }, [])
+
+  return (
+    <AuthContext.Provider
+      value={{
+        signIn,
+        loading,
+        user,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
