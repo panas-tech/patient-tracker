fef58165ce926036a8064817df6af8607f1f621b
Napoleon Salazar <nsalazar0429@gmail.com>
2020-12-26 16:44:43 -0500

    feat: add auth user id from snapshot abd format functions code using prettier

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,42 +3,52 @@ import * as admin from 'firebase-admin'
 
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
-  databaseURL: 'https://patient-tracker-d45ae.firebaseio.com'
-});
-
-export const registerDoctor = functions.firestore.document('doctors/{doctorId}').onCreate(async (snap, context) => {
-  
-  const newValue = snap.data();
-
-  await admin.auth().createUser({
-    email:newValue.email,
-    emailVerified: false,
-    phoneNumber: newValue.phoneNumber,
-    password: 'password',
-    displayName: `${newValue.firstName} ${newValue.lastName}`,
-    disabled: false,
-  }).then(() =>{
-
-    console.log(` ${newValue.firstName} was successfully registered` ,newValue)
-
-  }).catch((error) => {
-
-    console.log(` ERROR - ${error}` ,newValue)
-  })
+  databaseURL: 'https://patient-tracker-d45ae.firebaseio.com',
 })
 
-export const deleteDoctor = functions.firestore.document('doctors/{doctorId}').onDelete(async (snap, context) => {
-  const deletedValue = snap.data();
+export const registerDoctor = functions.firestore
+  .document('doctors/{doctorId}')
+  .onCreate(async (snap, context) => {
+    const newValue = snap.data()
 
-  const user = await admin.auth().getUserByEmail(deletedValue.email)
-
-  await admin
-  .auth()
-  .deleteUser(user.uid)
-  .then(() => {
-    console.log(`${deletedValue.firstName} ${deletedValue.lastName} was deleted from auth system`);
+    await admin
+      .auth()
+      .createUser({
+        uid: snap.id,
+        email: newValue.email,
+        emailVerified: false,
+        phoneNumber: newValue.phoneNumber,
+        password: 'password',
+        displayName: `${newValue.firstName} ${newValue.lastName}`,
+        disabled: false,
+      })
+      .then(() => {
+        console.log(
+          ` ${newValue.firstName} was successfully registered`,
+          newValue
+        )
+      })
+      .catch((error) => {
+        console.log(` ERROR - ${error}`, newValue)
+      })
   })
-  .catch((error) => {
-    console.log(`ERROR -> ${error}`);
-  });
-})
+
+export const deleteDoctor = functions.firestore
+  .document('doctors/{doctorId}')
+  .onDelete(async (snap, context) => {
+    const deletedValue = snap.data()
+
+    const user = await admin.auth().getUserByEmail(deletedValue.email)
+
+    await admin
+      .auth()
+      .deleteUser(user.uid)
+      .then(() => {
+        console.log(
+          `${deletedValue.firstName} ${deletedValue.lastName} was deleted from auth system`
+        )
+      })
+      .catch((error) => {
+        console.log(`ERROR -> ${error}`)
+      })
+  })
